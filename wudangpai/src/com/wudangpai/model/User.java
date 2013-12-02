01ec72840c90859087b1ac6563aa0b33d9170254
sxfszwr <sxfszwr@126.com>
2013-12-02 22:43:41 +0800

    first commit

---
+++ wudangpai/src/com/wudangpai/model/User.java
@@ -0,0 +1,148 @@
+package com.wudangpai.model;
+
+import java.sql.Date;
+
+import javax.persistence.Entity;
+import javax.persistence.GeneratedValue;
+import javax.persistence.Id;
+
+@Entity
+public class User {
+	
+	private Integer id;
+	private String name;
+	private String password;
+	private String sex;
+	private String email;
+	private Date birthdate;
+	private String qq;
+	private String phone;
+	private String company;
+	private String city;
+	private String address;
+	private String description;
+	private String image_path;
+	private Date regist_time;
+	private Date last_login_time;
+	private Integer referrer_id;
+	private String college_year;
+	private String major;
+	
+	public User(){
+		
+	}
+	
+	
+	@Id
+	@GeneratedValue
+	public Integer getId() {
+		return id;
+	}
+	public void setId(Integer id) {
+		this.id = id;
+	}
+	public String getName() {
+		return name;
+	}
+	public void setName(String name) {
+		this.name = name;
+	}
+	public String getPassword() {
+		return password;
+	}
+	public void setPassword(String password) {
+		this.password = password;
+	}
+	public String getSex() {
+		return sex;
+	}
+	public void setSex(String sex) {
+		this.sex = sex;
+	}
+	public String getEmail() {
+		return email;
+	}
+	public void setEmail(String email) {
+		this.email = email;
+	}
+	public Date getBirthdate() {
+		return birthdate;
+	}
+	public void setBirthdate(Date birthdate) {
+		this.birthdate = birthdate;
+	}
+	public String getQq() {
+		return qq;
+	}
+	public void setQq(String qq) {
+		this.qq = qq;
+	}
+	public String getPhone() {
+		return phone;
+	}
+	public void setPhone(String phone) {
+		this.phone = phone;
+	}
+	public String getCompany() {
+		return company;
+	}
+	public void setCompany(String company) {
+		this.company = company;
+	}
+	public String getCity() {
+		return city;
+	}
+	public void setCity(String city) {
+		this.city = city;
+	}
+	public String getAddress() {
+		return address;
+	}
+	public void setAddress(String address) {
+		this.address = address;
+	}
+	public String getDescription() {
+		return description;
+	}
+	public void setDescription(String description) {
+		this.description = description;
+	}
+	public String getImage_path() {
+		return image_path;
+	}
+	public void setImage_path(String image_path) {
+		this.image_path = image_path;
+	}
+	public Date getRegist_time() {
+		return regist_time;
+	}
+	public void setRegist_time(Date regist_time) {
+		this.regist_time = regist_time;
+	}
+	public Date getLast_login_time() {
+		return last_login_time;
+	}
+	public void setLast_login_time(Date last_login_time) {
+		this.last_login_time = last_login_time;
+	}
+	public Integer getReferrer_id() {
+		return referrer_id;
+	}
+	public void setReferrer_id(Integer referrer_id) {
+		this.referrer_id = referrer_id;
+	}
+	public String getCollege_year() {
+		return college_year;
+	}
+	public void setCollege_year(String college_year) {
+		this.college_year = college_year;
+	}
+	public String getMajor() {
+		return major;
+	}
+	public void setMajor(String major) {
+		this.major = major;
+	}
+	
+
+}
