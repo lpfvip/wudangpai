a090d61a63e6bc692d89730977faedcb6753f695
sxfszwr <sxfszwr@126.com>
2013-12-05 14:20:44 +0800

    fix some fiedds and add a excel to description our db tables
    Signed-off-by: sxfszwr <sxfszwr@126.com>

--- src/main/resources/user.sql
+++ src/main/resources/user.sql
@@ -10,7 +10,7 @@ Target Server Type    : MYSQL
 Target Server Version : 50525
 File Encoding         : 65001
 
-Date: 2013-12-05 13:36:35
+Date: 2013-12-05 14:18:05
 */
 
 SET FOREIGN_KEY_CHECKS=0;
@@ -24,19 +24,19 @@ CREATE TABLE `user` (
   `address` varchar(255) DEFAULT NULL,
   `birthdate` date DEFAULT NULL,
   `city` varchar(255) DEFAULT NULL,
-  `college_year` varchar(255) DEFAULT NULL,
+  `collegeyear` varchar(255) DEFAULT NULL,
   `company` varchar(255) DEFAULT NULL,
   `description` varchar(255) DEFAULT NULL,
   `email` varchar(255) DEFAULT NULL,
-  `image_path` varchar(255) DEFAULT NULL,
-  `last_login_time` date DEFAULT NULL,
+  `imagepath` varchar(255) DEFAULT NULL,
+  `lastlogintime` date DEFAULT NULL,
   `major` varchar(255) DEFAULT NULL,
   `name` varchar(255) DEFAULT NULL,
   `password` varchar(255) DEFAULT NULL,
   `phone` varchar(255) DEFAULT NULL,
   `qq` varchar(255) DEFAULT NULL,
   `referrer_id` int(11) DEFAULT NULL,
-  `regist_time` date DEFAULT NULL,
+  `registtime` date DEFAULT NULL,
   `sex` varchar(255) DEFAULT NULL,
   PRIMARY KEY (`id`)
 ) ENGINE=InnoDB DEFAULT CHARSET=utf8;
