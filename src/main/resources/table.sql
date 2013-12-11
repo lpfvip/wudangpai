4be771c15f99e2153e37239485c27e072ca0dbb5
Petrie <lpfvip2008@sohu.com>
2013-12-11 09:40:14 +0800

    I refer 'Discuz'change USER table to Member. and I use 'mybatis-generator' generator the DAO and MODEL, Next it's 'biz' and 'controller' maybe tomrrow or after.

--- src/main/resources/table.sql
+++ src/main/resources/table.sql
@@ -12,28 +12,26 @@ File Encoding         : 65001
 
 Date: 2013-12-05 14:18:05
 */
-
-CREATE TABLE `pre_common_member` (
-  `uid` mediumint(8) unsigned NOT NULL AUTO_INCREMENT COMMENT '会员id',
-  `email` char(40) NOT NULL DEFAULT '' COMMENT '邮箱',
-  `username` char(15) NOT NULL DEFAULT '' COMMENT '用户名',
-  `password` char(32) NOT NULL DEFAULT '' COMMENT '密码',
-  `status` tinyint(1) NOT NULL DEFAULT '0' COMMENT '判断用户是否已经删除 需要discuz程序加判断，并增加整体清理的功能。',
-  `emailstatus` tinyint(1) NOT NULL DEFAULT '0' COMMENT 'email是否经过验证',
-  `avatarstatus` tinyint(1) NOT NULL DEFAULT '0' COMMENT '是否有头像 ',
-  `adminid` tinyint(1) NOT NULL DEFAULT '0' COMMENT '管理员id',
-  `groupid` smallint(6) unsigned NOT NULL DEFAULT '0' COMMENT '会员组id',
-  `groupexpiry` int(10) unsigned NOT NULL DEFAULT '0' COMMENT '用户组有效期',
-  `extgroupids` char(20) NOT NULL DEFAULT '' COMMENT '扩展用户组',
-  `regdate` int(10) unsigned NOT NULL DEFAULT '0' COMMENT '注册时间',
-  `credits` int(10) NOT NULL DEFAULT '0' COMMENT '总积分',
-  `newpm` smallint(6) unsigned NOT NULL DEFAULT '0' COMMENT '新短消息数量',
-  `newprompt` smallint(6) unsigned NOT NULL DEFAULT '0' COMMENT '新提醒数目',
-  `onlyacceptfriendpm` tinyint(1) NOT NULL DEFAULT '0' COMMENT '是否只接收好友短消息',
+CREATE DATABASE  IF NOT EXISTS `wudangpai`;
+CREATE TABLE  IF NOT EXISTS `pre_common_member`(
+  `uid` mediumint(8) unsigned NOT NULL AUTO_INCREMENT COMMENT 'member id', 
+  `email` char(40) NOT NULL DEFAULT '' COMMENT 'email',  
+  `username` char(15) NOT NULL DEFAULT '' COMMENT 'username', 
+  `password` char(32) NOT NULL DEFAULT '' COMMENT 'password',  
+  `status` tinyint(1) NOT NULL DEFAULT '0' COMMENT 'user deleted', 
+  `emailstatus` tinyint(1) NOT NULL DEFAULT '0' COMMENT 'email verified',
+  `avatarstatus` tinyint(1) NOT NULL DEFAULT '0' COMMENT 'user avatar ',
+  `adminid` tinyint(1) NOT NULL DEFAULT '0' COMMENT 'operator id', 
+  `groupid` smallint(6) unsigned NOT NULL DEFAULT '0' COMMENT 'member group id', 
+  `groupexpiry` int(10) unsigned NOT NULL DEFAULT '0' COMMENT 'user group validity',
+  `extgroupids` char(20) NOT NULL DEFAULT '' COMMENT 'expand user group', 
+  `regdate` int(10) unsigned NOT NULL DEFAULT '0' COMMENT 'registration time',
+  `credits` int(10) NOT NULL DEFAULT '0' COMMENT 'total credits', 
+  `newpm` smallint(6) unsigned NOT NULL DEFAULT '0' COMMENT 'new message number',
+  `newprompt` smallint(6) unsigned NOT NULL DEFAULT '0' COMMENT 'new prompt number',
+  `onlyacceptfriendpm` tinyint(1) NOT NULL DEFAULT '0' COMMENT 'accept friend message only', 
   PRIMARY KEY (`uid`),
   UNIQUE KEY `username` (`username`),
   KEY `email` (`email`),
-  KEY `groupid` (`groupid`),
-) ENGINE=MyISAM DEFAULT CHARSET=utf8 COMMENT='用户主表';
-
-
+  KEY `groupid` (`groupid`)
+) ENGINE=MyISAM DEFAULT CHARSET=utf8 COMMENT='Member' 
